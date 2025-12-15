5bc0d43f0a5dec0af48aa843a643ed598c72bcc6
Goomez <danielgdpaula@gmail.com>
2025-12-15 20:04:35 -0300

    update Error

--- src/main/java/com/mycompany/projectschoolinterface/BackEnd/addStatusStudent.java
+++ src/main/java/com/mycompany/projectschoolinterface/BackEnd/addStatusStudent.java
@@ -2,9 +2,8 @@ package com.mycompany.projectschoolinterface.BackEnd;
 
 import com.mycompany.projectschoolinterface.Class.Student.listStudent;
 import com.mycompany.projectschoolinterface.Class.Student.student;
-import com.mycompany.projectschoolinterface.FrontEnd.addDataStudent;
 
-public class addStatusStudent extends addDataStudent {
+public class addStatusStudent{
     public static student student = new student();
     public static listStudent listStudent = new listStudent();
     public static addStatusStudent addStatusStudent = new addStatusStudent();
